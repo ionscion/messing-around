e08bf10e29abd6a7c6cde60d5d09719d8ce31189
Benjamin Sboto <bensboto@benjamins-mbp.lan>
2022-10-19 19:03:45 -0600

    added js code,variables and a button

---
+++ script.js
@@ -0,0 +1,12 @@
+var x = 10;
+var y = 15;
+var Z = x + y;
+
+/*window.alert ("Warning: Entering Bread Zone");*/
+
+function myFunction() {
+    document.getElementById("myNote").innerHTML = "Working with a yeast starter is hard!";
+    document.getElementById("jsNote").innerHTML = "The value of random number plus Z is" + Z;
+
+}
+
